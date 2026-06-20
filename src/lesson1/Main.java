4e8fd5edbe7cf2991d18a5b69475bfb3fd7b8218
HrachyaPR <hrachya.baloyann@gmail.com>
2026-06-20 12:53:21 +0400

    init. lesson 1

---
+++ src/lesson1/Main.java
@@ -0,0 +1,34 @@
+package lesson1;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        char l='*';
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+
+        System.out.println("//////////////");
+        for (int i = 5; i >0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+        System.out.println("///////////");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                for (int k = 0; k < j+1; k++) {
+                    System.out.print("#");
+                }
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}

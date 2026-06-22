a65d33c4880795538c994dea3a19c81b817bc485
HrachyaPR <hrachya.baloyann@gmail.com>
2026-06-22 13:55:24 +0400

    message

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -22,10 +22,10 @@ public class Main {
         }
         System.out.println("///////////");
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                for (int k = 0; k < j+1; k++) {
-                    System.out.print("#");
-                }
+            for (int j = 0; j <5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2*(i-1); j++) {
                 System.out.print(l);
             }
             System.out.println();
